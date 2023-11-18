2ea599c87c0cb7df0c99c5999bfe238c7cc0bd28
bendehalasz <138763919+bendehalasz@users.noreply.github.com>
2023-11-19 00:12:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -62,22 +62,5 @@ function checkCredentials() {
     
 }
 
-var participants =  [
-    {'Name': 'Xavier Cameron-Higgs', 'Email': 'xavier.cameron-higgs@qneiform.com', 'Password': 'nutcracker14'},
-    {'Name': 'Kamilla Biró', 'Email': 'kamilla.biro@qneiform.com', 'Password': 'snowflake12'},
-    {'Name': 'Eszter Lőrincz', 'Email': 'eszter.lorincz@qneiform.com', 'Password': 'snowflake48'},
-    {'Name': 'Betti Szmutku', 'Email': 'bernadett.szmutku@qneiform.com', 'Password': 'snowflake24'},
-    {'Name': 'Kristina Stavri', 'Email': 'kristina.stavri@qneiform.com', 'Password': 'star35'},
-    {'Name': 'Anna Pálmai', 'Email': 'anna.palmai@qneiform.com', 'Password': 'garland66'},
-    {'Name': 'Federica Piezzo', 'Email': 'federica.piezzo@qneiform.com', 'Password': 'fireplace72'},
-    {'Name': 'Gergely Füstös', 'Email': 'gergely.fustos@qneiform.com', 'Password': 'star53'},
-    {'Name': 'Anna Ferenczy', 'Email': 'anna.ferenczy@qneiform.com', 'Password': 'gingerbread61'},
-    {'Name': 'Nándor Tóth', 'Email': 'nandor.toth@qneiform.com', 'Password': 'candle61'},
-    {'Name': 'Nóra Kovács', 'Email': 'nora.kovacs@qneiform.com', 'Password': 'nutcracker40'},
-    {'Name': 'Thomas Percy', 'Email': 'thomas.percy@qneiform.com', 'Password': 'wreath84'},
-    {'Name': 'Bence Bíró', 'Email': 'bence.biro@qneiform.com', 'Password': 'wreath10'},
-    {'Name': 'Bende Halasz', 'Email': 'bende.halasz@qneiform.com', 'Password': 'present75'},
-  ]
 
-var shuffled_names =  ['Kristina Stavri', 'Anna Pálmai', 'Nóra Kovács', 'Federica Piezzo', 'Gergely Füstös', 'Bende Halasz', 'Nándor Tóth', 'Eszter Lőrincz', 'Thomas Percy', 'Xavier Cameron-Higgs', 'Betti Szmutku', 'Kamilla Biró', 'Bence Bíró', 'Anna Ferenczy']
 

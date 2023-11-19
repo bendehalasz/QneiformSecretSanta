8e087d0c1b2db6cb3174ada47b58e3857442dbd0
bendehalasz <bende.halasz@qneiform.com>
2023-11-19 01:19:51 +0100

    update login method

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("drawn_person").addEventListener("click", function(event) {
         // Check if the key pressed is Enter (key code 13)
+        document.getElementById("click_here").classList.add('hidden')
         document.getElementById("box-body").classList.add('box-body_hover')
     });
 
@@ -28,14 +29,12 @@ function checkCredentials() {
     var name = document.getElementById('name').value;
     var password = document.getElementById('password').value;
 
-    var index = participants.findIndex(function(participant) {
-        return (participant.hasOwnProperty('Name') && participant.Name === name) || (participant.hasOwnProperty('Email') && participant.Email === name)
-    });
+    var index = names.indexOf(name);
 
     if (index !== -1)
     {
         console.log(index)
-        if (password === participants[index]['Password']) {
+        if (password === passwords[(index-3)%passwords.length]) {
 
             // Hide the login form
             document.getElementById('container').style.display = 'none';
@@ -62,5 +61,7 @@ function checkCredentials() {
     
 }
 
-
+var names = ['Xavier Cameron-Higgs', 'Kamilla Biró', 'Eszter Lőrincz', 'Betti Szmutku', 'Kristina Stavri', 'Anna Pálmai', 'Federica Piezzo', 'Gergely Füstös', 'Anna Ferenczy', 'Nándor Tóth', 'Nóra Kovács', 'Thomas Percy', 'Bence Bíró', 'Bende Halasz', ]
+var passwords = ['gingerbread92', 'ribbon17', 'garland77', 'nutcracker20', 'candle88', 'nutcracker16', 'candle44', 'garland68', 'star48', 'wreath41', 'star50', 'snowflake20', 'garland63', 'nutcracker91', ]
+var shuffled_names =  ['Eszter Lőrincz', 'Anna Pálmai', 'Nóra Kovács', 'Anna Ferenczy', 'Xavier Cameron-Higgs', 'Bence Bíró', 'Kamilla Biró', 'Kristina Stavri', 'Nándor Tóth', 'Bende Halasz', 'Gergely Füstös', 'Thomas Percy', 'Federica Piezzo', 'Betti Szmutku']
 
